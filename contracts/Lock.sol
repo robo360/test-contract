7a2b48a7f273d5226c5067483149d7fd78cbd7fe
Roberto  Berwa <berwa05@gmail.com>
2023-04-06 09:20:51 -0700

    change when to time

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -8,7 +8,7 @@ contract Lock {
     uint public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event Withdrawal(uint amount, uint time);
 
     constructor(uint _unlockTime) payable {
         require(
